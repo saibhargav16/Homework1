fa78eab9ceb7a1066cca8591890b60c0c6bc9709
saibhargav16 <77900761+saibhargav16@users.noreply.github.com>
2021-01-23 23:33:34 -0500

    Add files via upload

---
+++ grades.py
@@ -0,0 +1,23 @@
+def grade(mark):
+    if mark >= 95 and mark<= 100:
+        return 'A+'
+    elif (mark >= 90 and mark <= 94):
+        return 'A'
+    elif (mark >= 85 and mark <= 89):
+        return 'A-'
+    elif (mark >= 80 and mark <= 84):
+        return 'B+'
+    else:
+        return 'Invalid Input'
+
+
+while(True):
+    cal = int(input('Enter the value:'))
+    if cal == 0:
+        break
+    else:
+        print(grade(cal))
+   
+
+
+
